f80a4c63371c2c64a758b7280ec36dddab0c8f8b
rohitkoshyari <rohitkoshyari7244@gmail.com>
2026-03-24 00:04:03 +0530

    removed some bug and replaced whole backend with spring bood instead of node js

--- backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-const { requireAuth } = require('../middleware/auth');
-
-router.post('/login', authController.login);
-router.get('/me', requireAuth, authController.me);
-
-module.exports = router;
